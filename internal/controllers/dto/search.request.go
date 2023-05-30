c8a953fea377ea2d947b54214aa2b673db899530
Trương Phú Hùng <hungtruong@cim.com.vn>
2023-05-30 12:19:05 +0700

    Revert "fix: camelcase to snakecase"
    This reverts commit 6bf99c948297a5f5c0a098c8c9c9d253758b0a93.

--- internal/controllers/dto/search.request.go
+++ internal/controllers/dto/search.request.go
@@ -5,7 +5,7 @@ type SearchNftReq struct {
 	Token      string `query:"token" validate:"omitempty,eth_addr"`
 	Owner      string `query:"owner" validate:"omitempty,eth_addr"`
 	Identifier string `query:"identifier" validate:"omitempty"`
-	IsHidden   *bool  `query:"is_hidden"`
+	IsHidden   *bool  `query:"isHidden"`
 	Offset     int32  `query:"offset" validate:"gte=0"`
 	Limit      int32  `query:"limit" validate:"gte=0,lte=100"`
 }
