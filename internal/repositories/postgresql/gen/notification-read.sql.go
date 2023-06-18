7eeff8a9837b840850259c759e55b520eb9cc0f0
Pham Phuoc <phhphc@gmail.com>
2023-06-18 15:18:29 +0700

    fix: fix get notification error

--- internal/repositories/postgresql/gen/notification-read.sql.go
+++ internal/repositories/postgresql/gen/notification-read.sql.go
@@ -14,8 +14,8 @@ const getNotification = `-- name: GetNotification :many
 SELECT n.is_viewed, n.info, n.event_name, n.order_hash, n.address,
     e.token, e.token_id, e.quantity, e.type, e.price, e.from, e.to, e.date,
     nft.owner,
-    CAST(nft.metadata ->> 'image' AS VARCHAR) AS nft_image,
-	CAST(nft.metadata ->> 'name' AS VARCHAR) AS nft_name
+    COALESCE(nft.metadata ->> 'image', '')::VARCHAR AS nft_image,
+	COALESCE(nft.metadata ->> 'name', '')::VARCHAR AS nft_name
 FROM "notifications" n
 JOIN "events" e ON n.event_name = e.name AND n.order_hash = e.order_hash
 JOIN "nfts" nft ON e.token = nft.token AND e.token_id = CAST(nft.identifier AS varchar(78))
